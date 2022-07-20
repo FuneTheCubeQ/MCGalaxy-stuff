e368e987ffa7d0801ed21d0780971d9436413b39
Nin <79152040+xXNinjaKingXx@users.noreply.github.com>
2022-07-20 00:59:53 -0400

    Update CmdMeow.cs

--- CmdMeow.cs
+++ CmdMeow.cs
@@ -1,67 +1,59 @@
-using MCGalaxy.Commands.Chatting;
-using System;
-using System.Threading;
+// █▀▀ █▀█ █▀▀ ▄▀█ ▀█▀ █▀█ █▀█ ▀
+// █▄▄ █▀▄ ██▄ █▀█ ░█░ █▄█ █▀▄ ▄
 
-namespace MCGalaxy
+
+// ███╗░░██╗██╗███╗░░██╗░░░░░██╗░█████╗║
+// ████╗░██║██║████╗░██║░░░░░██║██╔══██║
+// ██╔██╗██║██║██╔██╗██║░░░░░██║███████║
+// ██║╚████║██║██║╚████║██╗░░██║██╔══██║
+// ██║░╚███║██║██║░╚███║╚█████╔╝██║░░██║
+
+
+using System;
+using System.Text;
+using MCGalaxy;
+using MCGalaxy.Tasks;
+using MCGalaxy.Util;
+
+namespace MCGalaxy.Commands.Fun
 {
-	public class CmdMeow : MessageCmd
-	{
-		public override string name { get { return "Meow"; } }
-		public override string shortcut { get { return ""; } }
-		public override string type { get { return "moderation"; } }
-		public override bool museumUsable { get { return false; } }
-		public override LevelPermission defaultRank { get { return LevelPermission.Guest; } }
-		public override void Use(Player p, string message)
-		{
-			if (!CanSpeak(p, message))
-            		{
+    public class CmdMeow : Command
+    {
+        public override string name { get { return "Meow"; } }
+        public override string shortcut { get { return ""; } }
+        public override string type { get { return "fun"; } }
+        public override bool museumUsable { get { return true; } }
+        public override LevelPermission defaultRank { get { return LevelPermission.Guest; } }
+        
+        static DateTime nextUse;
+        
+        public override void Use(Player p, string message)
+        {
+        {
+			TimeSpan delta = nextUse - DateTime.UtcNow;
+			if (delta.TotalSeconds > 0)
+			{
+				p.Message("&cYou have to wait another &b{0} &cseconds before using this command again.",
+									   (int)Math.Ceiling(delta.TotalSeconds));
 				return;
 			}
+			nextUse = DateTime.UtcNow.AddSeconds(5);
+            }
 
-            		{
-				string suffix = RandomMessage();
-				Chat.MessageChat(ChatScope.Global, p, p.ColoredName + " &7Used /Meow " + suffix, null, null);
-			}
-		}
-
-		public string RandomMessage()
-		{
-			Random random = new Random();
-			int val = random.Next(11);
-
-			switch(val)
-           		{
-				case 0:
-					return "%dbut instead, Gekale typed %dMEOW!";
-				case 1:
-					return "%dand meowed!";
-				case 2:
-					return "%dbut went to New Blood and did /banall instead.";
-				case 3:
-					return "%dbut on the wrong server.";
-				case 4:
-					return "%dbut died before typing it.";
-				case 5:
-					return "%dbut forgot Gekale didn't finish the code.";
-				case 6:
-					return "%dbut it didn't work.";
-				case 7:
-					return "%dbut became a dog instead.";
-				case 8:			  
-					return "%dbut exploded.";
-				case 9:
-					return "%dbut quacked instead.";
-				case 10:
-					return "%dbut went to NA2 instead.";
-				default:
-					return "%dbut went to Shad's store instead.";
-
-			}
-		}
-
-		public override void Help(Player p)
-		{
-			p.Message("/Meow - Meow like a cat.");
-		}
-	}
+            if (p.muted)
+            {
+                p.Message("%cYou cannot use this command while your muted.");
+                return;
+            }
+            else if (message == "")
+            { 
+            Chat.MessageFrom(p, $"{p.ColoredName}%6 MEOWED %elike a cat!"); 
+            }
+        }
+        public override void Help(Player p)
+        {
+            p.Message("&T/Meow");
+            p.Message("&T/Meow - Makes you Meow like a cat");
+        }
+    }
 }
